ea4abf92ed1e7d46103c666564739283990393e9
diomonogatari <17832206+diomonogatari@users.noreply.github.com>
2026-02-06 16:25:27 +0000

    refactor: Enhance ErrorResponse and PagedResults classes with XML documentation and nullable properties

--- src/Bitbucket.Net/Common/Models/ErrorResponse.cs
+++ src/Bitbucket.Net/Common/Models/ErrorResponse.cs
@@ -2,8 +2,14 @@
 
 namespace Bitbucket.Net.Common.Models
 {
+    /// <summary>
+    /// Represents the error response returned by the Bitbucket Server API.
+    /// </summary>
     public class ErrorResponse
     {
-        public IEnumerable<Error> Errors { get; set; }
+        /// <summary>
+        /// Gets or sets the collection of errors returned by the server.
+        /// </summary>
+        public IEnumerable<Error>? Errors { get; set; }
     }
 }
