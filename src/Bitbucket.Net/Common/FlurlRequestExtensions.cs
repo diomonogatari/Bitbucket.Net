a730d38594b3443149cf19199c438992042fc11b
diomonogatari <17832206+diomonogatari@users.noreply.github.com>
2026-02-06 16:21:22 +0000

    refactor: Update method signatures to support nullable parameters in BitbucketHelpers and FlurlRequestExtensions

--- src/Bitbucket.Net/Common/FlurlRequestExtensions.cs
+++ src/Bitbucket.Net/Common/FlurlRequestExtensions.cs
@@ -5,7 +5,7 @@ namespace Bitbucket.Net.Common
 {
     public static class FlurlRequestExtensions
     {
-        public static IFlurlRequest WithAuthentication(this IFlurlRequest request, Func<string> getToken, string userName, string password)
+        public static IFlurlRequest WithAuthentication(this IFlurlRequest request, Func<string>? getToken, string? userName, string? password)
         {
             if (getToken != null)
             {
